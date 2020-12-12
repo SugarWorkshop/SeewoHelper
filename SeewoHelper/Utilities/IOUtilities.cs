b331d5f0359f085679c0ccc0df8e88e649211d82
Ricky8955555 <397050061@qq.com>
2020-12-12 22:07:06 +0800

    基本实现课件整理

---
+++ SeewoHelper/Utilities/IOUtilities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SeewoHelper.Utilities
+{
+    internal static class IOUtilities
+    {
+        public static bool IsProperPath(string path)
+        {
+            var regex = new Regex(@"^[a-zA-Z]:(\\[0-9a-zA-Z]+)*\\?$");
+            return regex.IsMatch(path);
+        }
+
+        public static string PathAppend(string path, string append)
+        {
+            if (path.EndsWith("\\"))
+            {
+                return path + append;
+            }
+            else
+            {
+                return path + "\\" + append;
+            }
+        }
+    }
+}
