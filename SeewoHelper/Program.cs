3beefcf38f240fc7be03573a4f087f56b05a667e
Ricky8955555 <397050061@qq.com>
2021-01-01 12:16:40 +0800

    修复子线程异常无法捕获问题

--- SeewoHelper/Program.cs
+++ SeewoHelper/Program.cs
@@ -21,26 +21,26 @@ namespace SeewoHelper
         {
             Logger.Add(new Log("应用启动"));
 
-            Application.ThreadExit += Application_ThreadExit;
-            Application.ThreadException += Application_ThreadException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new WindowMain());
         }
 
-        private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
-        {
-            MessageBox.Show($"程序给你抛出了异常，异常消息：\n{e.Exception.Message}\n\n详细信息请查看日志，并提交 issue，有能力的话也可以发 pr 哦");
-            Logger.Add(new Log(e.Exception.ToString(), LogLevel.Error));
-        }
-
-        private static void Application_ThreadExit(object sender, EventArgs e)
+        private static void CurrentDomain_ProcessExit(object sender, EventArgs e)
         {
             foreach (var disposable in AutoDisposer)
             {
                 disposable.Dispose();
             }
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show($"程序给你抛出了异常，异常消息：\n{(e.ExceptionObject as Exception)?.Message ?? e.ExceptionObject}\n\n详细信息请查看日志，并提交 issue，有能力的话也可以发 pr 哦");
+            Logger.Add(new Log(e.ExceptionObject.ToString(), LogLevel.Error));
         }
     }
 }
