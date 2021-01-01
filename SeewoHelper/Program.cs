1b18b8a165ccadb75a853450be422c59ee1443e4
Ricky8955555 <397050061@qq.com>
2021-01-02 01:25:45 +0800

    修复 Merge 造成的 Bug

--- SeewoHelper/Program.cs
+++ SeewoHelper/Program.cs
@@ -2,9 +2,7 @@
 using SeewoHelper.Utilities;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace SeewoHelper
@@ -21,36 +19,26 @@ namespace SeewoHelper
         [STAThread]
         static void Main()
         {
-            if (!IsRunning())
+            var instance = InstanceUtilities.GetRunningInstance();
+
+            if (instance == null)
             {
                 Logger = new Logger(Path.Combine(Application.StartupPath, "Logs", DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".log"));
-
-                Logger.Add("应用启动");
 
                 AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
                 AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
 
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new WindowMain());
-            }
-        }
 
-            Process instance = InstanceUtilities.RunningInstance();
-            if (instance == null)
-            {
-                Logger = new Logger(Path.Combine(Application.StartupPath, "Logs", DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".log"));
                 Logger.Add("应用启动");
+
                 Application.Run(new WindowMain());
             }
             else
             {
-                MessageBox.Show("该应用已在运行");
-                var oldProcess = processes.Where(x => x.Id != currentProcess.Id).First();
-                NativeMethods.SwitchToThisWindow(oldProcess.MainWindowHandle, true);
+                InstanceUtilities.HandleRunningInstance(instance);
             }
-
-            return running;
         }
 
         private static void CurrentDomain_ProcessExit(object sender, EventArgs e)
