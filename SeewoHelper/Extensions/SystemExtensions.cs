e9d5e2c0f0436fdab7f0f36e788fe791603b3802
Ricky8955555 <397050061@qq.com>
2021-01-02 17:01:55 +0800

    修复异常捕获问题

---
+++ SeewoHelper/Extensions/SystemExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SeewoHelper
+{
+    internal static class SystemExtensions
+    {
+        public static Exception ShowAndLog(this Exception ex, Logger logger, bool terminating = false)
+        {
+            MessageBox.Show($"程序给你抛出了异常，异常消息：\n{ex.Message}\n\n详细信息请查看日志，并提交 issue，有能力的话也可以发 pr 哦");
+            logger.Add(ex.ToString(), terminating ? LogLevel.Fatal : LogLevel.Error);
+
+            return ex;
+        }
+    }
+}
