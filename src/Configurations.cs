d1003031e29c27bb8b246f77a97778f9850a613c
Ricky8955555 <397050061@qq.com>
2021-02-28 00:44:14 +0800

    优化

--- src/Configurations.cs
+++ src/Configurations.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using Sunny.UI;
+using System.IO;
 
 namespace SeewoHelper
 {
@@ -7,11 +8,11 @@ namespace SeewoHelper
         /// <summary>
         /// <see cref="SeewoHelper.CoursewareSortingInfo"/> 配置
         /// </summary>
-        public static readonly Configuration<CoursewareSortingInfo> CoursewareSortingInfo = new Configuration<CoursewareSortingInfo>(Path.Combine(Constants.ConfigurationPath, "CoursewareSortingInfo.json"), new CoursewareSortingInfo());
+        public static readonly Configuration<CoursewareSortingInfo> CoursewareSortingInfo = new Configuration<CoursewareSortingInfo>(Path.Combine(Constants.ConfigurationPath, "CoursewareSortingInfo.json"), new CoursewareSortingInfo(string.Empty, new()));
 
         /// <summary>
         /// <see cref="SeewoHelper.UISettings"/> 配置
         /// </summary>
-        public static readonly Configuration<UISettings> UISettings = new Configuration<UISettings>(Path.Combine(Constants.ConfigurationPath, "UISettings.json"), new UISettings());
+        public static readonly Configuration<UISettings> UISettings = new Configuration<UISettings>(Path.Combine(Constants.ConfigurationPath, "UISettings.json"), new UISettings(UIStyle.LightBlue, LogLevel.Info));
     }
 }
--- src/Models/UISettings.cs
+++ src/Models/UISettings.cs
@@ -5,5 +5,5 @@ namespace SeewoHelper
     /// <summary>
     /// 表示 UI 设置
     /// </summary>
-    public record UISettings(UIStyle Style = UIStyle.LightBlue, LogLevel LogLevel = LogLevel.Info);
+    public record UISettings(UIStyle Style, LogLevel LogLevel);
 }
